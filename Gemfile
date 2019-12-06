a1ce707c60adcd2144f8dbb10c30271cf1d8af20
Rafael Damas <rafael.damas99@gmail.com>
2019-12-06 09:49:19 -0300

    Correções no projeto. invertido os arquivos steps e features para pastas certas e correções de boas práticas no código

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
-source "https://rubygems.org"
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
 
 gem 'capybara'
 gem 'cpf_cnpj'
@@ -6,3 +8,4 @@ gem 'cucumber'
 gem 'httparty'
 gem 'pry-nav'
 gem 'rspec'
+gem 'rubocop'
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
-require "capybara"
-require "capybara/cucumber"
-require "capybara/rspec"
-require "cucumber"
-require "httparty"
-require "pry-nav"
+# frozen_string_literal: true
+
+require 'capybara'
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'cucumber'
+require 'httparty'
+require 'pry-nav'
